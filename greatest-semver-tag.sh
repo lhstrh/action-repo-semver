007a2acca72262bac5ccf05c4e4e7e10677ad60e
Marten Lohstroh <marten@berkeley.edu>
2022-05-01 12:47:26 -0700

    Adjustments

--- greatest-semver-tag.sh
+++ greatest-semver-tag.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-latest=""
+latest="0.0.0"
 while IFS= read -r line; do
     if [[ $(semver compare ${line} ${latest} 2> /dev/null) = '1' ]]; then
         latest=${line}
     fi
     #echo "... $line ..."
 done <<< "$(git tag)"
-echo $latest
+if [[ ${latest} != "0.0.0" ]]; then
+    echo ${latest}
+else
+   echo ""
+fi
